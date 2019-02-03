554a09680a595de123e965f8e4f0d232b09e8ca3
RedMage <amggamer@gmail.com>
2019-02-03 01:29:10 -0600

    Opens to add pages

--- minnehack-python/app.py
+++ minnehack-python/app.py
@@ -9,5 +9,30 @@ def indexpage():
     return render_template('index.html')
 
 
+@app.route('/farmadd')
+def farmaddpage():
+    return render_template('farmadd.html')
+
+
+@app.route('/fieldadd')
+def fieldaddpage():
+    return render_template('fieldadd.html')
+
+
+@app.route('/blockadd')
+def blockaddpage():
+    return render_template('blockadd.html')
+
+
+@app.route('/cropadd')
+def cropaddpage():
+    return render_template('cropadd.html')
+
+
+@app.route('/cropswap')
+def cropswappage():
+    return render_template('cropswap.html')
+
+
 if __name__ == '__main__':
     app.run()
