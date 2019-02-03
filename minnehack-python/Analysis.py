57386c652f684d4ceb492fb285f4c7d6d2ec9f38
eforberger <elsaforberger@icloud.com>
2019-02-02 23:49:29 -0800

    Commented functions

--- minnehack-python/Analysis.py
+++ minnehack-python/Analysis.py
@@ -10,11 +10,20 @@ class Analysis:
     def est_cost(self):
         pass
 
-    def est_yield_peracre(self):
-        pass
+    def get_yield_per_acre(self, cropt_string):
+        #finds the yeld per acre of the crop in input, and returns it
 
-    def convert(self, database_handle, crop_string):
+    def get_cost_per_ton(self, crop_string):
         pass
+    # From the FAOSTAT file, gets the cost in usd per ton of the input crop string
+
+    def create_crop (self, crop_string):
+        #creates a new crop using the cost_per_ton and _yield_per_acre methods.
+        #adds the crop to to database
+
+
+
+
 
 
 
