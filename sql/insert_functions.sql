c53088c38797018e7398a2a2ba322ceb71479146
Evan Merlock <vnmrlck@gmail.com>
2019-02-02 23:09:49 -0600

    added field insert function

--- sql/insert_functions.sql
+++ sql/insert_functions.sql
@@ -17,5 +17,14 @@ $$ LANGUAGE pgplsql;
 CREATE OR REPLACE FUNCTION orange_produce.fields(int, int, text) RETURNS int AS $$
 DECLARE
     temp_user_id       ALIAS FOR $1;
-    farm_name          ALIAS FOR $2;
-    
+    farm_id            ALIAS FOR $2;
+    field_name         ALIAS FOR $3;
+    field_id           int;
+BEGIN
+    INSERT INTO orange_produce.fields(id, name, contained_in, created_by)
+        VALUES (DEFAULT, field_name, farm_id, temp_user_id)
+        RETURNING id INTO field_id;
+    INSERT INTO orange_produce.field_audit(id, created_date, modified_date)
+        VALUES(field_id, now(), now());
+    RETURN farm_id;
+END;
