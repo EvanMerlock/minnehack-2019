6ac835262bcce322f93c94e780e6f164d7674531
Evan Merlock <vnmrlck@gmail.com>
2019-02-02 22:36:53 -0600

    added create table file

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,84 @@
 CREATE SCHEMA orange_produce;
 
 CREATE TABLE orange_produce.farms(
-    id      big_serial,
-    name    text NOT NULL,
+    id              big_serial,
+    name            text NOT NULL,
+    location        point NOT NULL,
+    created_by      bigint REFERENCES orange_produce.users ON DELETE CASCADE,
     primary_key(id)
 );
 
+CREATE TABLE orange_produce.farm_audit(
+    id              bigint REFERENCES orange_produce.farms ON DELETE CASCADE,
+    created_date    timestamptz NOT NULL,
+    modified_date   timestamptz NOT NULL,
+);
+
+CREATE TABLE orange_produce.fields(
+    id              big_serial,
+    name            text NOT NULL,
+    contained_in    bigint REFERENCES orange_produce.farms ON DELETE CASCADE,
+    created_by      bigint REFERENCES orange_produce.users ON DELETE CASCADE,
+    primary_key(id)
+);
+
+CREATE TABLE orange_produce.fields_audit(
+    id              bigint REFERENCES orange_produce.fields ON DELETE CASCADE,
+    created_date    timestamptz NOT NULL,
+    modified_date   timestamptz NOT NULL,
+);
+
+CREATE TABLE orange_produce.blocks(
+    id              big_serial,
+    name            text NOT NULL,
+    contained_in    bigint REFERENCES orange_produce.fields ON DELETE CASCADE,
+    created_by      bigint REFERENCES orange_produce.users ON DELETE CASCADE,
+    crops           bigint REFERENCES orange_produce.crops ON DELETE CASCADE,
+    primary_key(id)
+);
+
+CREATE TABLE orange_produce.blocks_audit(
+    id              bigint REFERENCES orange_produce.blocks ON DELETE CASCADE,
+    created_date    timestamptz NOT NULL,
+    modified_date   timestamptz NOT NULL,
+);
+
+CREATE TABLE orange_produce.crops(
+    id              big_serial,
+    name            text NOT NULL,
+    market_value    decimal NOT NULL,
+    yield_time      integer NOT NULL,
+    cost_per        decimal NOT NULL,
+    created_by      bigint REFERENCES orange_produce.users ON DELETE CASCADE,
+    primary_key(id)
+);
+
+CREATE TABLE orange_produce.crops_audit(
+    id              bigint REFERENCES orange_produce.crops ON DELETE CASCADE,
+    created_date    timestamptz NOT NULL,
+    modified_date   timestamptz NOT NULL, 
+);
+
+CREATE TABLE orange_produce.crop_swap_event(
+    id              big_serial,
+    previous_crop   bigint REFERENCES orange_produce.crops ON DELETE CASCADE,
+    new_crop        bigint REFERENCES orange_produce.crops ON DELETE CASCADE,
+    swap_date       timestamptz NOT NULL,
+);
+
+CREATE TABLE orange_produce.crop_event_audit(
+    id              bigint REFERENCES orange_produce.crop_swap_event ON DELETE CASCADE,
+    created_date    timestamptz NOT NULL,
+    modified_date   timestamptz NOT NULL,
+)
+
+CREATE TABLE orange_produce.users(
+    id              big_serial,
+    name            text NOT NULL,
+);
+
+CREATE TABLE orange_produce.users_audit(
+    id              bigint REFERENCES orange_produce.users ON DELETE CASCADE,
+    created_date    timestamptz NOT NULL,
+    modified_date   timestamptz NOT NULL, 
+);
