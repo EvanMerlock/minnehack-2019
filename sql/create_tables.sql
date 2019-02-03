05dde7ffcf7891340eecb306458c8c6b69be0050
Evan Merlock <vnmrlck@gmail.com>
2019-02-03 00:27:14 -0600

    finished insert functions

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -33,7 +33,7 @@ CREATE TABLE orange_produce.blocks(
     name            text NOT NULL,
     contained_in    bigint REFERENCES orange_produce.fields ON DELETE CASCADE,
     created_by      bigint REFERENCES orange_produce.users ON DELETE CASCADE,
-    crops           bigint REFERENCES orange_produce.crops ON DELETE CASCADE,
+    crop           bigint REFERENCES orange_produce.crops ON DELETE CASCADE,
     primary_key(id)
 );
 
@@ -46,6 +46,7 @@ CREATE TABLE orange_produce.blocks_audit(
 CREATE TABLE orange_produce.crops(
     id              big_serial,
     name            text NOT NULL,
+    country         text,
     market_value    money NOT NULL,
     yield_time      integer NOT NULL,
     cost_per        money NOT NULL,
@@ -64,6 +65,8 @@ CREATE TABLE orange_produce.crop_swap_event(
     previous_crop   bigint REFERENCES orange_produce.crops ON DELETE CASCADE,
     new_crop        bigint REFERENCES orange_produce.crops ON DELETE CASCADE,
     swap_date       timestamptz NOT NULL,
+    created_by      bigint REFERENCES orange_produce.users ON DELETE CASCADE,
+    primary_key(id)
 );
 
 CREATE TABLE orange_produce.crop_event_audit(
@@ -75,10 +78,5 @@ CREATE TABLE orange_produce.crop_event_audit(
 CREATE TABLE orange_produce.users(
     id              big_serial,
     name            text NOT NULL,
-);
-
-CREATE TABLE orange_produce.users_audit(
-    id              bigint REFERENCES orange_produce.users ON DELETE CASCADE,
-    created_date    timestamptz NOT NULL,
-    modified_date   timestamptz NOT NULL, 
+    primary_key(id)
 );
--- sql/insert_functions.sql
+++ sql/insert_functions.sql
@@ -14,7 +14,7 @@ BEGIN
 END;
 $$ LANGUAGE pgplsql;
 
-CREATE OR REPLACE FUNCTION orange_produce.fields(int, int, text) RETURNS int AS $$
+CREATE OR REPLACE FUNCTION orange_produce.insert_field(int, int, text) RETURNS int AS $$
 DECLARE
     temp_user_id       ALIAS FOR $1;
     farm_id            ALIAS FOR $2;
@@ -28,3 +28,57 @@ BEGIN
         VALUES(field_id, now(), now());
     RETURN farm_id;
 END;
+$$ LANGUAGE pgplsql;
+
+CREATE OR REPLACE FUNCTION orange_produce.insert_block(int, int, text, int) RETURNS int AS $$
+DECLARE
+    temp_user_id       ALIAS FOR $1;
+    field_id           ALIAS FOR $2;
+    block_name         ALIAS FOR $3;
+    crop_id            ALIAS FOR $4;
+    block_id           int;
+BEGIN
+    INSERT INTO orange_produce.blocks(id, name, contained_in, created_by, crop)
+        VALUES(DEFAULT, block_name, field_id, temp_user_id, crop_id)
+        RETURNING id INTO  block_id;
+    INSERT INTO orange_produce.blocks_audit(id, created_date, modified_date)
+        VALUES(DEFAULT, now(), now());
+    RETURN block_id;
+END;
+$$ LANGUAGE pgplsql;
+
+CREATE OR REPLACE FUNCTION orange_produce.insert_crop(int, text, money, integer, money, text DEFAULT NULL) RETURNS int AS $$
+DECLARE
+    temp_user_id        ALIAS FOR $1;
+    crop_name           ALIAS FOR $2;
+    market_value        ALIAS FOR $3;
+    yield_time          ALIAS FOR $4;
+    cost_per_unit       ALIAS FOR $5;
+    country             ALIAS FOR $6;
+    crop_id             int;
+BEGIN
+    INSERT INTO orange_produce.crops(id, name, country, market_value, yield_time, cost_per, created_by)
+        VALUES(DEFAULT, crop_name, country, market_value, yield_time, cost_per_unit, temp_user_id)
+        RETURNING id into crop_id;
+    INSERT INTO orange_produce.crops_audit(id, created_date, modified_date)
+        VALUES(crop_id, now(), now());
+    RETURN crop_id;
+END;
+$$ LANGUAGE pgplsql;
+
+CREATE OR REPLACE FUNCTION orange_produce.insert_crop_swap_event(int, int, int, timestamptz) RETURNS int AS $$
+DECLARE
+    temp_user_id        ALIAS FOR $1;
+    prev_crop_id        ALIAS FOR $2;
+    next_crop_id        ALIAS FOR $3;
+    swap_date           ALIAS FOR $4;
+    crop_swap_id        int;
+BEGIN
+    INSERT INTO orange_produce.crop_swap_event(id, previous_crop, new_crop, swap_date, created_by)
+        VALUES (DEFAULT, prev_crop_id, new_crop_id, swap_date, temp_user_id)
+        RETURNING id INTO crop_swap_id;
+    INSERT INTO orange_produce.crop_event_audit(id, created_date, modified_date)
+        VALUES(crop_swap_id, now(), now());
+    RETURN crop_swap_id;
+END;
+$$ LANGUAGE pgplsql;
