a0aac8002fc1e401dc4b78d01550f7d9d8bab69c
Evan Merlock <vnmrlck@gmail.com>
2019-02-03 11:42:10 -0600

    maybe rendering home page

--- minnehack-python/app.py
+++ minnehack-python/app.py
@@ -7,9 +7,53 @@ app = Flask(__name__)
 db = db_access.Database(psycopg2.connect())
 
 
-@app.route('/')
-def index_page():
-    return render_template('index.html')
+@app.route('/', methods=['POST', 'GET'])
+def index_page(request):
+    if request.method == "GET":
+        farms = db.get_all_farms()
+        return render_template('index.html', farms=farms, fields=[], blocks=[])
+    else:
+        if "farmupdate" in request.form:
+            farms = db.get_all_farms()
+            fields = db.get_fields_from_farm(int(request.form['farmselect'].replace("farm_", "")))
+            return render_template('index.html', farms=farms, fields=fields, blocks=[])
+        elif "farmedit" in request.form:
+            pass
+        elif "fieldremove" in request.form:
+            selected_farm = int(request.form['farmselect'].replace("farm_", ""))
+            selected_field = int(request.form['fieldselect'].replace("field_", ""))
+            db.remove_field(selected_field)
+            farms = db.get_all_farms()
+            fields = db.get_fields_from_farm(selected_farm)
+            return render_template('index.html', farms=farms, fields=fields, blocks=[])
+        elif "fieldupdate" in request.form:
+            farms = db.get_all_farms()
+            fields = db.get_fields_from_farm(int(request.form['farmselect'].replace("farm_", "")))
+            blocks = db.get_blocks_from_field(int(request.form['fieldselect'].replace("field_", "")))
+            return render_template('index.html', farms=farms, fields=fields, blocks=blocks)
+        elif "fieldedit" in request.form:
+            pass
+        elif "blockremove" in request.form:
+            farms = db.get_all_farms()
+            fields = db.get_fields_from_farm(int(request.form['farmselect'].replace("farm_", "")))
+            db.remove_block(int(request.form['blockselect'].replace("block_", "")))
+            blocks = db.get_blocks_from_field(int(request.form['fieldselect'].replace("field_", "")))
+            return render_template('index.html', farms=farms, fields=fields, blocks=blocks)
+        elif "blockedit" in request.form:
+            pass
+        elif "blockupdate" in request.form:
+            farms = db.get_all_farms()
+            fields = db.get_fields_from_farm(int(request.form['farmselect'].replace("farm_", "")))
+            blocks = db.get_blocks_from_field(int(request.form['fieldselect'].replace("field_", "")))
+            return render_template('index.html', farms=farms, fields=fields, blocks=blocks)
+        else:
+            # assume farmdelete
+            selected_farm = int(request.form['farmselect'].replace("farm_", ""))
+            db.remove_farm(selected_farm)
+            farms = db.get_all_farms()
+            return render_template('index.html', farms=farms, fields=[], blocks=[])
+
+
 
 
 @app.route('/farmadd')
